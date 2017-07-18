8807d556f857a93b11abed9a322981c6ca61fec8
xuejike <xuejike2004@126.com>
2017-07-18 10:14:46 +0800

    异步控制器结果

--- src/main/java/com/bidanet/hprose/starter/tool/AsyncTool.java
+++ src/main/java/com/bidanet/hprose/starter/tool/AsyncTool.java
@@ -7,12 +7,16 @@ import org.springframework.web.context.request.async.DeferredResult;
  * Created by xuejike on 2017/7/18.
  */
 public class AsyncTool {
-    DeferredResult asyncControllerResult(Promise promise,Long timeout,Object timeObj){
+    public static DeferredResult asyncControllerResult(Promise promise,Long timeout,Object timeObj){
         DeferredResult deferredResult=new DeferredResult(timeout,timeObj);
         promise.then(q->{
             deferredResult.setResult(q);
         });
+
         return deferredResult;
+    }
+    public static DeferredResult asyncControllerResult(Promise promise){
+        return asyncControllerResult(promise,3*1000L,"请求超时");
     }
 
 }
